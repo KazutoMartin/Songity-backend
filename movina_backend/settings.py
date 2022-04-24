75219bb4ee46ee4d5de93942585ea63420b98576
KazutoMartin <75247334+KazutoMartin@users.noreply.github.com>
2022-04-24 21:54:10 +0430

    deploy

--- movina_backend/settings.py
+++ movina_backend/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 # CORS
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
        'http://localhost:3001',
@@ -148,10 +148,7 @@ CORS_ORIGIN_WHITELIST = (
        'https://songity-backend.herokuapp.com',
 )
 ALLOWED_HOSTS = [
-    'http://localhost:3001',
-       'http://127.0.0.1:3001',
-       'https://songity-front.herokuapp.com',
-       'https://songity-backend.herokuapp.com',
+'*'
        ]
 
 # Rest Framework
